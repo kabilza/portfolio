44302ed324ae02db23c47b9a499a2ab31dddb721
kabilza <naruoluvohina@gmail.com>
2021-01-12 22:58:00 +0700

    initial commit

---
+++ portfolio/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from datetime import datetime
+import datetime as dt
+
+myDate = datetime.now()
+formatedDate = myDate.strftime("%Y-%m-%d %H:%M:%S")
+
+# Create your models here.
+
+class Info(models.Model):
+    """User's Info"""
+    gender = models.CharField('Gender (F or M)', max_length=1)
+    birthday = models.DateField(null=True, blank=True)
+    brief_info = models.TextField('Background Infomation', default="NOT SET")
+    phone_num = models.CharField('Phone Number', max_length=10, default="NOT SET")
+    twitter = models.TextField('Twitter', default="NOT SET")
+
+    def get_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_age(self):
+        return int((dt.date.today() - self.birthday).days/365.25)
+
+    def get_gender(self):
+        return self.gender
+
+    def get_brief_info(self):
+        return self.brief_info
+
+    def get_number(self):
+        return self.phone_num
+
+class Projects(models.Model):
+    """User's past projects and works"""
+    project_name = models.TextField('Project Name', default="NOT SET")
